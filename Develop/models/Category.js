7b48e11f076038ca60dd58506cc0d0f70efb1ffa
Tyrrance <tyrrancem37@gmail.com>
2023-04-27 17:24:44 -0400

    Initialized the Category model and setup the ID properties.

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -4,9 +4,10 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
-
+// Initialize the Category model with its properties and options
 Category.init(
   {
+   // The ID property is an auto-incrementing integer and the primary key for the model 
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
